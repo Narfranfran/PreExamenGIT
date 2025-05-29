09b3bd11ce811d84a0378be1625b48f686bc1237
Fran Santos <francisco.sangue.1@educa.jcyl.es>
2025-05-29 12:53:05 +0200

    Añadidos comentarios sobre conexion con github

--- holamundo.py
+++ holamundo.py
@@ -4,4 +4,7 @@ print('Hola mundo!')
     git init (en la carpeta donde quieras iniciar el repositorio)
     git add . (añade todo al repositorio)
     git commit -m "Mensaje"
+    git commit -a -m "Mensaje" (-a añade todo)
+    git remote add origin "enlace del repositorio"
+    git push origin master (master=nombre rama principal)
 '''
